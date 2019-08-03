ac89b9ef2a15eabe9464adacbeb7eecb26bed7ad
VladimirSuvorov1996 <51405835+VladimirSuvorov1996@users.noreply.github.com>
2019-08-03 14:26:36 +0300

    Delete RefreshBasicObserver.hpp

--- RefreshBasicObserver.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "WindowDependentBasicObserver.hpp"
-namespace graFX::input {
-	template<typename Implementation, auto invoke>
-	using WindowRefreshBasicObserver = WindowDependentBasicObserver<Implementation, invoke, void>;
-}
