6974f7edf7fe38a0f625c8b628b18264d826831b
VladimirSuvorov1996 <51405835+VladimirSuvorov1996@users.noreply.github.com>
2019-08-03 14:26:17 +0300

    Delete ScrollBasicObserver.hpp

--- ScrollBasicObserver.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "WindowDependentBasicObserver.hpp"
-namespace graFX::input {
-	template<typename Implementation, auto invoke>
-	using ScrollBasicObserver = WindowDependentBasicObserver<Implementation, invoke, void, callbacks::offset_x, callbacks::offset_y>;
-}
