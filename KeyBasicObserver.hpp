f928f548bf3043bd8354cb6bc282822069009d64
VladimirSuvorov1996 <51405835+VladimirSuvorov1996@users.noreply.github.com>
2019-08-03 14:33:03 +0300

    Delete KeyBasicObserver.hpp

--- KeyBasicObserver.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "WindowDependentBasicObserver.hpp"
-namespace graFX::input {
-	template<typename Implementation, auto invoke>
-	using KeyBasicObserver = WindowDependentBasicObserver<Implementation, invoke, void, callbacks::key, callbacks::system_specific_scancode, callbacks::action, callbacks::mode_bit_fields>;
-}
