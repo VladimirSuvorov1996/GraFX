1244dfb389a458ff15962de97203e928c58e808b
VladimirSuvorov1996 <51405835+VladimirSuvorov1996@users.noreply.github.com>
2019-09-01 20:41:38 +0300

    Delete File_s_DroppedBasicObserver.hpp

--- include/api/File_s_DroppedBasicObserver.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "WindowDependentBasicObserver.hpp"
-namespace graFX::input {
-	template<typename Implementation, auto invoke>
-	using File_s_DroppedBasicObserver = WindowDependentBasicObserver<Implementation, invoke, void, callbacks::files_count, callbacks::path_es>;
-}
