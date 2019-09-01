310d4cec860cee34b13aaf60a2e8154ddf91cbb8
VladimirSuvorov1996 <51405835+VladimirSuvorov1996@users.noreply.github.com>
2019-09-01 20:42:09 +0300

    Delete CharBasicObserver.hpp

--- include/api/CharBasicObserver.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "WindowDependentBasicObserver.hpp"
-namespace graFX::input {
-	template<typename Implementation, auto invoke>
-	using CharBasicObserver = WindowDependentBasicObserver<Implementation, invoke, void, callbacks::character_unicode_codepoint>;
-}
-
