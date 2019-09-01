903f610d16bcb1d97fae92c649c3d17ff4e8301a
VladimirSuvorov1996 <51405835+VladimirSuvorov1996@users.noreply.github.com>
2019-09-01 20:42:38 +0300

    Delete CursorMovedBasicObserver.hpp

--- include/api/CursorMovedBasicObserver.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "WindowDependentBasicObserver.hpp"
-namespace graFX::input {
-	template<typename Implementation, auto invoke>
-	using CursorMovedBasicObserver = WindowDependentBasicObserver<Implementation, invoke, void, callbacks::mouse_moved_to_x, callbacks::mouse_moved_to_y>;
-}
