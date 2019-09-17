28f3fe5a9c1b2997c1d6997ecca0c2b889bc50b3
VladimirSuvorov1996 <51405835+VladimirSuvorov1996@users.noreply.github.com>
2019-09-17 22:33:38 +0300

    Delete ContentScaledBasicObserver.hpp

--- api/basic_observers/ContentScaledBasicObserver.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "basic/WindowDependentBasicObserver.hpp"
-namespace graFX::input {
-	template<typename Implementation, auto invoke>
-	using WindowContentScaledBasicObserver = WindowDependentBasicObserver<Implementation, invoke, void, callbacks::to_scale_x, callbacks::to_scale_y>;
-}
