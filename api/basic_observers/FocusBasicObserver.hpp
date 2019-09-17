4dce3dd758a460a09e17947f7b5556a4a94d479c
VladimirSuvorov1996 <51405835+VladimirSuvorov1996@users.noreply.github.com>
2019-09-17 22:34:23 +0300

    Delete FocusBasicObserver.hpp

--- api/basic_observers/FocusBasicObserver.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "basic/WindowDependentBasicObserver.hpp"
-namespace graFX::input {
-	template<typename Implementation, auto invoke>
-	using WindowFocusBasicObserver = WindowDependentBasicObserver<Implementation, invoke, void, callbacks::is_focused>;
-}
