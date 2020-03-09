631525fdcedd6a0ffa9b8b01a2814fedf53f70cf
VladimirSuvorov1996 <51405835+VladimirSuvorov1996@users.noreply.github.com>
2020-03-09 19:30:23 +0300

    Delete CloseBasicObserver.hpp

--- include/api/basic_observers/CloseBasicObserver.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "basic/WindowDependentBasicObserver.hpp"
-namespace graFX::input {
-	template<typename Implementation, auto invoke>
-	using WindowCloseBasicObserver = WindowDependentBasicObserver<Implementation, invoke, void>;
-}
