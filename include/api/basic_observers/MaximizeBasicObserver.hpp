a831ec22a06e3b2c1d842fc530943650b7e1f2aa
VladimirSuvorov1996 <51405835+VladimirSuvorov1996@users.noreply.github.com>
2020-03-09 19:28:12 +0300

    Delete MaximizeBasicObserver.hpp

--- include/api/basic_observers/MaximizeBasicObserver.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "basic/WindowDependentBasicObserver.hpp"
-namespace graFX::input {
-	template<typename Implementation, auto invoke>
-	using WindowMaximizeBasicObserver = WindowDependentBasicObserver<Implementation, invoke, void, callbacks::is_maximized>;
-}
