0d490bca55d7bdd9f835da218812f2e7410f6e81
VladimirSuvorov1996 <51405835+VladimirSuvorov1996@users.noreply.github.com>
2020-03-09 19:27:42 +0300

    Delete MouseButtonBasicObserver.hpp

--- include/api/basic_observers/MouseButtonBasicObserver.hpp
+++
@@ -1,7 +0,0 @@
-#pragma once
-#include "basic/WindowDependentBasicObserver.hpp"
-#include "../implementation/WindowMouse.hxx"
-namespace graFX::input {
-	template<typename Implementation, auto invoke>
-	using MouseButtonBasicObserver = WindowDependentBasicObserver<Implementation, invoke, void, callbacks::button, callbacks::is_pressed, callbacks::mode_bit_fields>;
-}
