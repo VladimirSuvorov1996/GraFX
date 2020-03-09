2dab0a25e18f90f48965287e57073ae2f79494b2
VladimirSuvorov1996 <51405835+VladimirSuvorov1996@users.noreply.github.com>
2020-03-09 19:27:33 +0300

    Delete MoveBasicObserver.hpp

--- include/api/basic_observers/MoveBasicObserver.hpp
+++
@@ -1,6 +0,0 @@
-#pragma once
-#include "basic/WindowDependentBasicObserver.hpp"
-namespace graFX::input {
-	template<typename Implementation, auto invoke>
-	using WindowMoveBasicObserver = WindowDependentBasicObserver<Implementation, invoke, void, callbacks::to_x, callbacks::to_y>;
-}
